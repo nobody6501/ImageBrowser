db791b893694b865e3728ab32e3a44859b9569ea
nobody6501 <nobody6501@utexas.edu>
2015-05-09 02:14:19 -0500

    imageholder class/ adapter/ browse UIxml

---
+++ app/src/main/java/martin/imagebrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,9 @@
+package martin.imagebrowser;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Ultimate on 5/9/2015.
+ */
+public class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<ImageHolder> {
+}
---
+++ app/src/main/java/martin/imagebrowser/ImageHolder.java
@@ -0,0 +1,21 @@
+package martin.imagebrowser;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Ultimate on 5/9/2015.
+ */
+public class ImageHolder extends RecyclerView.ViewHolder {
+
+    protected ImageView thumbnail;
+    protected TextView title;
+
+    public ImageHolder(View view) {
+        super(view);
+        this.thumbnail = (ImageView)view.findViewById(R.id.thumbnail);
+        //this.title = (TextView)view.findViewById(R.id.title);
+    }
+}
